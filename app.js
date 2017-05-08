a3540a6cef58f7498caefc1bece840e1099cd7d5
cale312 <zikisacalesogaxa@gmail.com>
2017-05-08 15:39:47 +0200

    used beautify from atom

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var greetingsController = require('./controllers/greetingsController');
 var app = express();
 
 app.use('/public', express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 greetingsController(app);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+  console.log('Our app is running on http://localhost:' + port);
 });
