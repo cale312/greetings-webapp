ebadda8a3e215ada3a9e3cba1a88ba9457c4cffe
cale312 <zikisacalesogaxa@gmail.com>
2017-05-08 15:25:18 +0200

    other commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var greetingsController = require('./controllers/greetingsController');
 var app = express();
--- controllers/greetingsController.js
+++ controllers/greetingsController.js
@@ -1,16 +1,17 @@
 'use strict';
 module.exports = function(app) {
   var mongoose = require('mongoose');
+
+  const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/greetings";
+  mongoose.connect(mongoURL);
+
   var db = mongoose.connection;
 
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
     console.log('We are connected');
   });
-
-  const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/greetings";
-  mongoose.connect(mongoURL);
-
+  
   var greetSchema = mongoose.Schema({
     name: String,
     greetCount: Number
