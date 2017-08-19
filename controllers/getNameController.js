1376871ddeb7532357e0c308c8102bb2d4afeb49
cale312 <zikisacalesogaxa@gmail.com>
2017-08-19 15:37:34 +0200

    added conditional statements for when a name already exists or not in theditional statement, for handling non duplication in the database

--- controllers/getNameController.js
+++ controllers/getNameController.js
@@ -1,6 +1,36 @@
 const greeted = require('../models/nameSchemaModel');
 
-//looks for the name in the database
+var namesObj = {};
+
 module.exports = function(theName) {
+
+    if (namesObj[theName] !== undefined) {
+
+        namesObj[theName] += 1;
+
+        greeted.findOneAndUpdate({
+            name: theName
+        }, {
+            $inc: {
+                greetCount: 1
+            }
+        }).then(function(result) {
+            console.log('Update done')
+        }).catch(function(err) {
+            console.log(err);
+        });
+    } else {
+
+        namesObj[theName] = 1;
+
+        greeted.create({
+           name: theName,
+           greetCount: 1
+        }).then(function(result) {
+            console.log('New name added!');
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
     return theName
 }
